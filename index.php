4c066a41bea8b21fb34a8a335f97ea0f661bb9fb
Gabriel <eltongraniero@gmail.com>
2026-03-06 22:53:30 -0300

    Segundo commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Nosso Futebol</title>
+    <link rel="stylesheet" href="dash.css">
+</head>
+
+<body>
+    <div class="dashboard-container">
+        <nav class="menu">
+            <h2>Bem vindo ao melhor site sobre o mundo da bola!</h2>
+            <ul>
+           <li><a href="#">Inicio</a></li>
+           <li><a href="#">Campeonatos</a></li>
+           <li><a href="#">Notícias</a></li>
+           <li><a href="#">Estatísticas</a></li>
+
+           <div class="perfil-usuario">
+    <img src="https://ui-avatars.com/api/?name=Gabriel+Della&background=008080&color=fff" alt="Avatar">
+    <span>Gabriel</span>
+</div>
+           
+</ul>
+
+</nav>
+<main>
+    ,<section class="container-cards">
+        <article>
+            <h2>Campeonatos</h2>
+            <p>Veja Todos os Campeonatos</p>
+            <a href="##" class="btn">Acessar</a>
+        </article>
+
+        <article>
+            <h2>Uefa Champions League</h2>
+            <p>Veja a classificação da Uefa Champions League</p>
+            <a href="##" class="btn">Acessar</a>
+        </article>
+
+        <article>
+           <h2>Campeonato Brasileiro</h2>
+           <p>Veja a classificação do Campeonato Brasileiro</p>
+         <a href="##" class="btn">Acessar</a>
+        </article
+    </section>
+</main>
+</div>
+      <footer>
+    <p>&copy; <?php echo date ('Y'); ?> - Desenvolvido na aula de Web I</p>
+</footer>
+
+            
+        
+
+
+    
+</body>
+</html>
