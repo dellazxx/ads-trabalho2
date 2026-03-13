a72f3c9d20b0628b7af8aeb7b76ab674d9787cf5
Gabriel <eltongraniero@gmail.com>
2026-03-13 20:40:46 -0300

    feat: implementada tabela de gestão de usuários

---
+++ usuarios.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestão de Usuários</title>
+    <link rel="stylesheet" href="usuarios.css">
+ 
+    
+
+</head>
+<body>
+    <main>
+        <div class="header-content">
+            <h2>Gestão de Usuários</h2>
+            <p>Visualize e gerencie as permissões dos Usuáriosdo sistema.</p>
+</div>
+
+<div class="table-responsive">
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Acesso</th>
+                <th>Status</th>
+                <th>Ações</th>
+</tr>
+</thead>
+<tbody>
+    <tr>
+
+       <td>01</td>
+        <td>Gabriel Della</td>
+        <td>eltongraniero@gmail.com</td>
+        <td>Administrador</td>
+        <td><span class="badge ativo">Ativo</span></td>
+        <td><button class="btn-icon"><i class="fa-solid fa-pen-to-square"></i></button></td>
+</tr>
+<tr>
+    <td>02</td>
+    <td>Mariane Karen</td>
+    <td>mariane.karen@gmail.com</td>
+    <td>Editor</td>
+    <td><span class="badge ativo">Ativo</span></td>
+    <td><button class="btn-icon"><i class="fa-solid fa-pen-to-square"></i><button></td>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> 
+</tr>
+</tbody>
+</table>
+</div>
+    </main>
+    
+</body>
+</html>
